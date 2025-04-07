5ee67bc35fa7b7d124ebdd3073119241ec05c76f
Max-Sam231 <maxmarutin64649@gmail.com>
2025-04-07 19:03:56 +0600

    chore: add modal window

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import styles from "./Modal.module.scss";
+import Image from "next/image";
+
+interface Props {
+	children: React.ReactNode;
+	isOpen: boolean;
+	setIsOpen: (open: boolean) => void;
+}
+
+const Modal: React.FC<Props> = ({ setIsOpen, isOpen, children }) => {
+	return (
+		<>
+			{isOpen ? (
+				<div className={styles.modal}>
+					<div className={styles.modal__container}>
+						<div className={styles.modal__header}>
+							<span className={styles.icon}>
+								<Image
+									src="/assets/svg/warning.svg"
+									alt="Logo"
+									width={100}
+									height={100}
+									style={{ width: "auto", height: "auto", background: "transparent" }}
+								/>
+							</span>
+							<p className={styles["text--sl"]}>Обратите внимание</p>
+						</div>
+						<div className={styles.modal__content}>{children}</div>
+						<div className={styles.modal__footer}>
+							<button className={`${styles.button} ${styles["button--active"]}`}>
+								Подтвердить
+							</button>
+							<button onClick={() => setIsOpen(!isOpen)} className={styles.button}>
+								Отменить
+							</button>
+						</div>
+					</div>
+					<div onClick={() => setIsOpen(!isOpen)} className={styles.modal__wrapper}></div>
+				</div>
+			) : (
+				""
+			)}
+		</>
+	);
+};
+export default Modal;
