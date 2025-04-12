e7cfb0ad0bae5cd28a1b55bb090e68e9d166fd43
Max-Sam231 <maxmarutin64649@gmail.com>
2025-04-12 13:18:49 +0600

    fix: toggle display

--- src/components/ControlPanel/ControlPanel.tsx
+++ src/components/ControlPanel/ControlPanel.tsx
@@ -3,10 +3,11 @@ import Image from "next/image";
 import styles from "./ControlPanel.module.scss";
 import React, { useContext } from "react";
 import { ModalsContext } from "@/app/reservoirs/[id]/page";
+import { useReservoirStore } from "@/store/reservoirsStore";
 
 const ControlPanel: React.FC = () => {
 	const { lockModal, setLockModal, deleteModal, setDeleteModal } = useContext(ModalsContext);
-
+	const {selectedReservoir} = useReservoirStore();
 	return (
 		<div className={styles.controlPanel}>
 			<div className={styles.inputContainer}>
@@ -22,7 +23,7 @@ const ControlPanel: React.FC = () => {
 				<input
 					className={styles.inputContainer__input}
 					type="text"
-					value={lockModal ? "Заблокирован" : "Разблокирован"}
+					value={selectedReservoir?.isLocked ? "Заблокирован" : "Разблокирован"}
 					disabled
 					readOnly
 				/>
@@ -30,7 +31,7 @@ const ControlPanel: React.FC = () => {
 					<input
 						type="checkbox"
 						name="toggle"
-						checked={lockModal}
+						checked={selectedReservoir?.isLocked}
 						readOnly
 						onClick={() => setLockModal(!lockModal)}
 						className={styles.toggle__button}
