a9dd2f68585dbdd6fc984955acc8f00c5f7b7241
Max-Sam231 <maxmarutin64649@gmail.com>
2025-04-15 20:09:29 +0600

    feat: add ability create reservoir

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,35 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Header.module.scss";
 import Image from "next/image";
+import CreateReservoir from "../CreateReservoir/CreateReservoir";
 
 type Props = {
-	setIsOpenMobile :()=> void;
-}
+  setIsOpenMobile: () => void;
+};
 
-const Header: React.FC<Props> = ({setIsOpenMobile}) => {
-	return (
-		<div className={styles.header}>
-			<div className={styles.left}>
-				<div className={styles.logo}>
-					<span className={styles.icon}>
-						<Image
-							src="/assets/svg/settings.svg"
-							alt="Logo"
-							width={25}
-							height={25}
-						/>
-					</span>
-					<p className={styles["text--sl"]}>Панель администрирования резервуаров</p>
-				</div>
-				<button className={`${styles.button} ${styles["text--sl"]}`}>Добавить резервуар</button>
-			</div>
-			<div className={styles.right}>
-				<button className={`${styles.button} ${styles["text--sl"]}`}>Экран резервуаров</button>
-				<button className={styles.mobileMenu} onClick={() => setIsOpenMobile()}>
-					<Image src="/assets/svg/mobile-icon.svg" alt="Logo" width={24} height={24} />
-				</button>
-			</div>
-		</div>
-	);
+const Header: React.FC<Props> = ({ setIsOpenMobile }) => {
+  const [isOpenCreate, setIsOpenCreate] = useState(false);
+  return (
+    <div className={styles.header}>
+      <div className={styles.left}>
+        <div className={styles.logo}>
+          <span className={styles.icon}>
+            <Image src="/assets/svg/settings.svg" alt="Logo" width={25} height={25} />
+          </span>
+          <p className={styles["text--sl"]}>Панель администрирования резервуаров</p>
+        </div>
+        <button
+          onClick={() => setIsOpenCreate(true)}
+          className={`${styles.button} ${styles["text--sl"]}`}
+        >
+          Добавить резервуар
+        </button>
+        <CreateReservoir isOpen={isOpenCreate} setIsOpen={setIsOpenCreate} />
+      </div>
+      <div className={styles.right}>
+        <button className={`${styles.button} ${styles["text--sl"]}`}>Экран резервуаров</button>
+        <button className={styles.mobileMenu} onClick={() => setIsOpenMobile()}>
+          <Image src="/assets/svg/mobile-icon.svg" alt="Logo" width={24} height={24} />
+        </button>
+      </div>
+    </div>
+  );
 };
 export default Header;
--- src/components/ReservoirInfo/ReservoirInfo.tsx
+++ src/components/ReservoirInfo/ReservoirInfo.tsx
@@ -10,116 +10,115 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { updateReservoir } from "@/api/reservoirs";
 
 const ReservoirInfo: React.FC = () => {
-	const { selectedReservoir, setSelectedReservoir, fetchReservoirs } = useReservoirStore();
-	const methods = useForm<ReservoirFormData>({
-		resolver: zodResolver(reservoirSchema),
-		defaultValues: {
-			name: selectedReservoir?.name || "",
-			capacity: selectedReservoir?.capacity || 0,
-			volume: selectedReservoir?.volume || 0,
-			productId: selectedReservoir?.productId || 1,
-		},
-		mode: "onChange",
-	});
+  const { selectedReservoir, setSelectedReservoir, fetchReservoirs } = useReservoirStore();
+  const methods = useForm<ReservoirFormData>({
+    resolver: zodResolver(reservoirSchema),
+    defaultValues: {
+      name: selectedReservoir?.name || "",
+      capacity: selectedReservoir?.capacity || 0,
+      volume: selectedReservoir?.volume || 0,
+      productId: selectedReservoir?.productId || 1,
+    },
+    mode: "onChange",
+  });
 
-	const { reset, handleSubmit } = methods;
+  const { reset, handleSubmit } = methods;
 
-	const measures: string[] = ["ТОННЫ", "%"];
-	const [activeMeasure, setActiveMeasure] = useState(measures[0]);
+  const measures: string[] = ["ТОННЫ", "%"];
+  const [activeMeasure, setActiveMeasure] = useState(measures[0]);
 
-	useEffect(() => {
-		if (selectedReservoir) {
-			reset({
-				name: selectedReservoir.name,
-				capacity: selectedReservoir.capacity,
-				volume: selectedReservoir.volume,
-				productId: selectedReservoir.productId,
-			});
-		}
-	}, [selectedReservoir, reset]);
+  useEffect(() => {
+    if (selectedReservoir) {
+      reset({
+        name: selectedReservoir.name,
+        capacity: selectedReservoir.capacity,
+        volume: selectedReservoir.volume,
+        productId: selectedReservoir.productId,
+      });
+    }
+  }, [selectedReservoir, reset]);
 
-	const handleSuccess = (name: keyof ReservoirFormData) => {
-		const currentValue = methods.getValues(name);
-		setSelectedReservoir({
-			...selectedReservoir!,
-			[name]: name === "capacity" || name === "volume" ? Number(currentValue) : currentValue,
-		});
-		methods.setValue(name, currentValue, { shouldDirty: false });
-	};
+  const handleSuccess = (name: keyof ReservoirFormData) => {
+    const currentValue = methods.getValues(name);
+    setSelectedReservoir({
+      ...selectedReservoir!,
+      [name]: name === "capacity" || name === "volume" ? Number(currentValue) : currentValue,
+    });
+    methods.setValue(name, currentValue, { shouldDirty: false });
+  };
 
-	const onSubmit: SubmitHandler<ReservoirFormData> = async (data) => {
-		if (methods.formState.isDirty) {
-			console.log("Form submitted:", data);
-			return;
-		}
-		console.log("Form submitted:", data);
-		const updatedReservoir = await updateReservoir(selectedReservoir?.id, data);
-		await fetchReservoirs();
-		setSelectedReservoir(updatedReservoir);
-	};
+  const onSubmit: SubmitHandler<ReservoirFormData> = async (data) => {
+    if (methods.formState.isDirty === true || methods.formState.isValid === false) {
+      return;
+    }
+    console.log("Form submitted:", data);
+    const updatedReservoir = await updateReservoir(selectedReservoir?.id, data);
+    await fetchReservoirs();
+    setSelectedReservoir(updatedReservoir);
+  };
 
-	return (
-		<FormProvider {...methods}>
-			<form className={styles.reservoirInfo} onSubmit={handleSubmit(onSubmit)}>
-				<div className={styles.reservoirInfo__container}>
-					<Input
-						icon="tank-icon"
-						name="name"
-						error={methods.formState.errors.name?.message}
-						onCancel={(name) => methods.resetField(name)}
-						onSuccess={(name) => handleSuccess(name)}
-						defaultValue={selectedReservoir?.name || ""}
-					/>
-					<Select name="productId" control={methods.control} placeholder="Выберите продукт" />
-					<Input
-						icon="capacity-full"
-						name="capacity"
-						type="number"
-						error={methods.formState.errors.capacity?.message}
-						onCancel={(name) => methods.resetField(name)}
-						onSuccess={(name) => handleSuccess(name)}
-						defaultValue={selectedReservoir?.capacity || 0}
-					/>
+  return (
+    <FormProvider {...methods}>
+      <form className={styles.reservoirInfo} onSubmit={handleSubmit(onSubmit)}>
+        <div className={styles.reservoirInfo__container}>
+          <Input
+            icon="tank-icon"
+            name="name"
+            error={methods.formState.errors.name?.message}
+            onCancel={(name) => methods.resetField(name)}
+            onSuccess={(name) => handleSuccess(name)}
+            defaultValue={selectedReservoir?.name || ""}
+          />
+          <Select name="productId" control={methods.control} placeholder="Выберите продукт" />
+          <Input
+            icon="capacity-full"
+            name="capacity"
+            type="number"
+            error={methods.formState.errors.capacity?.message}
+            onCancel={(name) => methods.resetField(name)}
+            onSuccess={(name) => handleSuccess(name)}
+            defaultValue={selectedReservoir?.capacity || 0}
+          />
 
-					<div>
-						{measures.map((item, index) => (
-							<button
-								key={index}
-								type="button"
-								onClick={() => setActiveMeasure(item)}
-								className={`${styles.button} ${
-									activeMeasure === item ? styles["button--active"] : ""
-								} ${styles.reservoirInfo__buttonSwitch}`}
-							>
-								{item}
-							</button>
-						))}
-						<Input
-							icon="capacity"
-							name="volume"
-							type="number"
-							error={methods.formState.errors.volume?.message}
-							onCancel={(name) => methods.resetField(name)}
-							onSuccess={(name) => handleSuccess(name)}
-							defaultValue={selectedReservoir?.volume || 0}
-						/>
-					</div>
-					<div className={styles.submitContainer}>
-						<button type="submit" className={`${styles.button} ${styles["button--active"]}`}>
-							Сохранить
-						</button>
-						<button
-							type="button"
-							className={`${styles.button} ${styles["button--disable"]}`}
-							onClick={() => methods.reset()}
-						>
-							Отменить
-						</button>
-					</div>
-				</div>
-			</form>
-		</FormProvider>
-	);
+          <div>
+            {measures.map((item, index) => (
+              <button
+                key={index}
+                type="button"
+                onClick={() => setActiveMeasure(item)}
+                className={`${styles.button} ${
+                  activeMeasure === item ? styles["button--active"] : ""
+                } ${styles.reservoirInfo__buttonSwitch}`}
+              >
+                {item}
+              </button>
+            ))}
+            <Input
+              icon="capacity"
+              name="volume"
+              type="number"
+              error={methods.formState.errors.volume?.message}
+              onCancel={(name) => methods.resetField(name)}
+              onSuccess={(name) => handleSuccess(name)}
+              defaultValue={selectedReservoir?.volume || 0}
+            />
+          </div>
+          <div className={styles.submitContainer}>
+            <button type="submit" className={`${styles.button} ${styles["button--active"]}`}>
+              Сохранить
+            </button>
+            <button
+              type="button"
+              className={`${styles.button} ${styles["button--disable"]}`}
+              onClick={() => methods.reset()}
+            >
+              Отменить
+            </button>
+          </div>
+        </div>
+      </form>
+    </FormProvider>
+  );
 };
 
 export default ReservoirInfo;
